7c4c511f1df5a8b463344932089a2f042667c8e6
raul <raul11@freenet.de>
2024-11-14 22:38:57 +0100

    Use esp32c3 build for all riscv ESPs

--- extra_script.py
+++ extra_script.py
@@ -5,6 +5,10 @@ from os.path import join, realpath, exists
 # For ESP this is BOARD_MCU
 cpu = env.get("BOARD_MCU")
 
+#All ESP32 except the original and S-series use riscv cores. Can use ESP32-C3 build.
+if len(cpu) > 5 and cpu[:5] == "esp32" and cpu[5] != "s":
+    cpu = "esp32c3"
+
 # For ARM cores, BOARD_MCU is the chip (e.g. rp2040), not the cpu (e.g. cortex-m0plus).
 # To find the correct binary, we check the linkflags.
 linkflags = env.get("LINKFLAGS", [])
